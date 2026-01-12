e4226eaecb8333b5e0f1ff429a8e10043ebb436b
satyajit-marmeto <satyajit.l@marmeto.com>
2026-01-12 18:28:36 +0530

    Add files via upload

---
+++ Technect/src/Components/DataArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import img from "../assets/construction.png"
+
+const DataArea = () => {
+  return (
+    <div className='home_box'>
+        <img src={img} alt="home" />
+        <h6>Home</h6>
+    </div>
+  )
+}
+
+export default DataArea
